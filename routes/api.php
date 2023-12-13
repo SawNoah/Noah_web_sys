5c4ab92dc6853e4d7384daf4b6c092b102f2f7d6
Saw Noah <sawnoah2002@gmail.com>
2023-12-13 11:22:35 +0630

    change api name

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/cookies", [CookieController::class, 'get_cookies']);
+Route::get("/test_employees", [EmployeeController::class, 'get_employees']);
 Route::post("/cookies", [CookieController::class, 'create_cookie']);
 
 Route::get("/test-employee", function () {
